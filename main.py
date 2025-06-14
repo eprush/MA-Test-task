899b01804539bb1c326fa44e24127f2f298f8b42
Egor Pavlovich <pavlovich.er@phystech.edu>
2025-06-14 20:50:19 +0300

    implement get_cookie

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-from faker import Faker
+from fake_useragent import UserAgent
+import requests
+from requests import Response
 
-def get_data():
-    ...
+from schemas import CookieSchema
+
+
+def get_cookie(r: Response) -> str:
+    cookie_string = r.headers["Set-Cookie"]
+    cookie_string = cookie_string.split(";")
+    return cookie_string[0][3:]
+
+def get_data() -> CookieSchema:
+    entry_url = "https://sentry.lenta.tech"
+    entry_headers = {
+        "Proxy-Authorization": "Basic",
+        "Proxy-Connection": "keep-alive",
+        "User-Agent": UserAgent(platforms="mobile").random
+    }
+
+    response = requests.get(entry_url, headers=entry_headers)
+    if response.status_code != 200:
+        raise ValueError(f"Invalid status code: {response.status_code}")
+    h = response.headers
+    result = {
+        "content_type": h["Content-Type"],
+        "connection": h["Connection"],
+        "vary": h["Vary"],
+        "cookie": get_cookie(response),
+    }
+    return CookieSchema(**result)
 
 def parse_data(*args):
     ...
@@ -16,4 +43,5 @@ def main():
     ...
 
 if __name__ == "__main__":
-    main()
+    data = get_data()
+    print(data)
